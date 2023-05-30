99c097e06c0b23bbc1e61eb43ae3f1e73713df05
Serafin Piña <arespin23@gmail.com>
2023-05-30 13:44:06 +0200

    cambios en cambios

--- actualizar_webs.php
+++ actualizar_webs.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <?php include "./uis/head.php" ?>
     <link rel="stylesheet" href="./styles/menu_lateral.css">
@@ -8,25 +9,54 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Actualizar Webs</title>
 </head>
+
 <body>
     <?php include "./uis/menu-lateral.php" ?>
     <section>
-        <div class="container mt-3">
-            
+        <div class="container mt-3 d-flex flex-row justify-content-evenly">
+
+            <div class="card" style="width: 18rem;">
+                <img src="./imagenes/iconos/original-baby.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+
+            <div class="card" style="width: 18rem;">
+                <img src="./imagenes/iconos/dulce-paseo.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+
+            <div class="card" style="width: 18rem;">
+                <img src="./imagenes/iconos/happy-way.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+
+
         </div>
     </section>
-    
 
-    <script>    
 
-    document.getElementById("boton-hamburguesa").addEventListener("click", function() {
-        var menu = document.getElementById("menu-hamburguesa");
-        if (menu.style.display === "none") {
-            menu.style.display = "block";
-        } else {
-            menu.style.display = "none";
-        }
-    });
-</script>
+    <script>
+        document.getElementById("boton-hamburguesa").addEventListener("click", function() {
+            var menu = document.getElementById("menu-hamburguesa");
+            if (menu.style.display === "none") {
+                menu.style.display = "block";
+            } else {
+                menu.style.display = "none";
+            }
+        });
+    </script>
 </body>
+
 </html>
---
+++ datos/cambios.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 30-05-2023 a las 13:35:10
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `almacen`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cambios`
+--
+
+CREATE TABLE `cambios` (
+  `model` varchar(64) NOT NULL,
+  `ean` varchar(14) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `price` decimal(14,2) NOT NULL,
+  `fecha_cambio` datetime NOT NULL,
+  `up_ropadecu` tinyint(1) DEFAULT NULL,
+  `up_babyline` tinyint(1) DEFAULT NULL,
+  `up_happy` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `cambios`
+--
+
+INSERT INTO `cambios` (`model`, `ean`, `quantity`, `price`, `fecha_cambio`, `up_ropadecu`, `up_babyline`, `up_happy`) VALUES
+('111', '1112', 41, 545.00, '2023-05-30 12:31:52', 0, 0, 0);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
